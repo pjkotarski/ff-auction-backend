965c12ebdc9cbae32fa2ccb418d7ebc6facca052
Pierce Kotarski <pjkotarski@gmail.com>
2021-11-11 16:06:49 -0600

    adds cron, resolves week, creates new collections on new weeks

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,22 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import './database/connect';
 import BidsRepo from './database/repo/Bids.repo';
 import IBid from './shared/types/IBid';
 import IPlayer from './shared/types/IPlayer';
-import { PositionEnum, InjuryStatusEnum } from './shared/enums/playerEnums';
 import PlayersRepo from './database/repo/Player.repo';
 import cron from 'node-cron';
-import { onCron } from './helpers/cron/cron.helper';
-import PlayerService from './services/PlayerService';
-
-
-dotenv.config();
+import WeekService from './services/WeekService';
 
 const port = process.env.SERVER_PORT;
 const app = express();
 
 // cron event triggered every Tuesday at 12:00 am
-cron.schedule('0 0 * * 2', () => onCron);
+cron.schedule('0 0 * * 2', () => WeekService.onCron);
 
 
-app.get('/restore-players', (req: any, res: any) => {
-    onCron();
-    res.send('restored players');
+app.get('/trigger-cron', (req: any, res: any) => {
+    WeekService.onCron();
+    res.send('imitate cron');
 })
 
 app.get('/add-bid', async(req: any, res: any) => {
@@ -45,16 +39,6 @@ app.get('/get-bids', async(req: any, res:any) => {
     return res.json(JSON.stringify(bids));
 });
 
-
-app.get('/all-players', async(req: any, res: any) => {
-
-
-    console.log('ATTEMPTING TO GET PLAYERS');
-    const freeAgents = await onCron();
-
-    res.json(freeAgents);
-
-});
 
 app.get('/get-players', async(req: any, res:any) => {
 
